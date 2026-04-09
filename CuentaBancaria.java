80e2a0d5751f1f22bcbde8738d09a4922d099483
xjose999 <josejoestar80@gmail.com>
2026-04-09 16:56:17 -0500

    Ejercicio 13

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -5,36 +5,32 @@ public class CuentaBancaria {
 
 
     public void mostrarCuenta() {
-        System.out.println("Su número de cuenta es " + numero);
-        System.out.println("Su saldo en la cuenta es de " + saldo);
-        System.out.println("El titular de la cuenta es " + titular);
+        System.out.println("---------------------------");
+        System.out.println("Titular: " + titular);
+        System.out.println("Número de cuenta: " + numero);
+        System.out.println("Saldo actual: $" + saldo);
+    }
 
+
+    public void consignar(double valor) {
+        saldo = saldo + valor;
+        System.out.println(">>> Se han consignado: $" + valor);
     }
 
     public static void main (String[] args) {
 
-        //PRIMERA CUENTA
         CuentaBancaria cuenta1 = new CuentaBancaria();
         cuenta1.numero = 9935247;
-        cuenta1.saldo = 220.000;
+        cuenta1.saldo = 220000.0;
         cuenta1.titular = "José Daniel Ballesteros";
 
 
-        //SEGUNDA CUENTA
-        CuentaBancaria cuenta2 = new CuentaBancaria();
-        cuenta2.numero = 7328124;
-        cuenta2.saldo = 100.000;
-        cuenta2.titular = "Manuel José Ballesteros Cantero";
-
-        cuenta2.saldo= 120.000;
-
-        System.out.println("Datos de la primera cuenta: ");
         cuenta1.mostrarCuenta();
 
-        System.out.println("Datos de la segunda cuenta");
-        cuenta2.mostrarCuenta();
+
+        cuenta1.consignar(50000.0);
 
 
+        cuenta1.mostrarCuenta();
     }
-
 }
