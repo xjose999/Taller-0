633f6e3e4eb85e9270b3678a79ebf548ce1fb091
xjose999 <josejoestar80@gmail.com>
2026-04-09 17:01:07 -0500

    Ejercicio 14

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -3,33 +3,38 @@ public class CuentaBancaria {
     double saldo;
     String titular;
 
-
     public void mostrarCuenta() {
-        System.out.println("---------------------------");
-        System.out.println("Titular: " + titular);
-        System.out.println("Número de cuenta: " + numero);
-        System.out.println("Saldo actual: $" + saldo);
+        System.out.println("Titular: " + titular + " | Saldo: $" + saldo);
     }
-
 
     public void consignar(double valor) {
         saldo = saldo + valor;
-        System.out.println(">>> Se han consignado: $" + valor);
+        System.out.println("Consignaste: $" + valor);
+    }
+
+
+    public void retirar(double valor) {
+        if (valor <= saldo) {
+            saldo = saldo - valor;
+            System.out.println("Retiro exitoso de: $" + valor);
+        } else {
+            System.out.println("Error: Saldo insuficiente. Intentaste retirar $" + valor + " pero solo tienes $" + saldo);
+        }
     }
 
     public static void main (String[] args) {
-
         CuentaBancaria cuenta1 = new CuentaBancaria();
         cuenta1.numero = 9935247;
-        cuenta1.saldo = 220000.0;
+        cuenta1.saldo = 100000.0;
         cuenta1.titular = "José Daniel Ballesteros";
-
 
         cuenta1.mostrarCuenta();
 
 
-        cuenta1.consignar(50000.0);
+        cuenta1.retirar(40000.0);
 
+
+        cuenta1.retirar(70000.0);
 
         cuenta1.mostrarCuenta();
     }
