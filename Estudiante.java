debf7e849d9e998ce1827690eae9ae2e105bf99a
xjose999 <josejoestar80@gmail.com>
2026-04-09 21:40:12 -0500

    Ejercicio 19

--- Estudiante.java
+++ Estudiante.java
@@ -1,3 +1,8 @@
+//Estudiante es una clase porque es el molde que usaremos para luego agregarle atributos y luego objetos,
+//en este caso los objetos pueden ser estudiante 1,2 o los que queramos por eso el de este ejemplo lleva el nombre
+// de est1
+
+
 public class Estudiante {
     String nombre;
     String codigo;
