98150d9412a44137964ddae9aa79950a3ab79087
xjose999 <josejoestar80@gmail.com>
2026-04-09 23:39:20 -0500

    Ejercicio 35 Actualizado

--- Lampara.java
+++ Lampara.java
@@ -2,23 +2,33 @@ public class Lampara {
     String marca;
     boolean encendida;
 
-    public Lampara() {
-        this.marca = "Generica";
+    public Lampara(String marca) {
+        this.marca = marca;
         this.encendida = false;
     }
 
-    public Lampara(String marca, boolean encendida) {
-        this.marca = marca;
-        this.encendida = encendida;
+    public void encender() {
+        encendida = true;
+    }
+
+    public void apagar() {
+        encendida = false;
     }
 
     public void mostrarEstado() {
         String estado = encendida ? "Encendida" : "Apagada";
-        System.out.println("Lampara marca: " + marca + " | Estado: " + estado);
+        System.out.println("La lampara " + marca + " esta " + estado);
     }
 
     public static void main(String[] args) {
-        Lampara miLampara = new Lampara("Philips", true);
+        Lampara miLampara = new Lampara("Xiaomi");
+
+        miLampara.mostrarEstado();
+
+        miLampara.encender();
+        miLampara.mostrarEstado();
+
+        miLampara.apagar();
         miLampara.mostrarEstado();
     }
 }
