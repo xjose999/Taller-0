e72605a762b5520819cc41fb4b1c0d055ac9b983
xjose999 <josejoestar80@gmail.com>
2026-04-09 22:57:41 -0500

    Ejercicio 23

--- Libro.java
+++ Libro.java
@@ -1,39 +1,22 @@
-//La clase es como un molde que define la ESTRUCTURA GENERAL y el objeto son casos ESPECIFICOS que se construyen a partir de ese molde
-
-
 public class Libro {
     String nombre;
     String genero;
 
+    public Libro() {
+        nombre = "Sin título";
+        genero = "Sin género";
+    }
+
     public void mostrarInfo() {
-        System.out.println("El nombre del libro es " + nombre);
-        System.out.println("El genero del libro es " + genero);
+        System.out.println("Libro: " + nombre + " | Género: " + genero);
     }
 
     public static void main(String[] args) {
-
-        Libro libr1 = new Libro();
-        {
-            libr1.nombre = "Alicia en el pais de las maravillas";
-            libr1.genero = "Infantil";
-
-            Libro libr2 = new Libro();
-            libr2.nombre = "Dune";
-            libr2.genero = "Fantasia";
-
-            Libro libr3 = new Libro();
-
-            libr3.nombre = "El Pistolero";
-            libr3.genero = "Ciencia Ficcion";
-
-            Libro libr4 = new Libro();
-
-            libr4.nombre = "Meridiano de sangre";
-            libr4.genero = "Salvaje Oeste";
+        Libro miLibro = new Libro();
+        miLibro.nombre = "Don Quijote";
+        miLibro.genero = "Clásico";
 
 
-        }
-
+        miLibro.mostrarInfo();
     }
-
 }
