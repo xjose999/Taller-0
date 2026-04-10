21b7c317886773f192d845ef972c2437f4532de1
xjose999 <josejoestar80@gmail.com>
2026-04-09 21:12:52 -0500

    Ejercicio 17

--- Estudiante.java
+++ Estudiante.java
@@ -3,26 +3,27 @@ public class Estudiante {
     String codigo;
     int semestre;
 
-
     public void mostrarInfo() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Código: " + codigo);
         System.out.println("Semestre: " + semestre + "°");
+    }
 
+
+    public void cambiarSemestre(int nuevoSemestre) {
+        semestre = nuevoSemestre;
+        System.out.println(">>> El semestre ha sido actualizado a: " + semestre + "°");
     }
 
     public static void main (String[] args) {
-
         Estudiante est1 = new Estudiante();
         est1.nombre = "José Daniel Ballesteros";
         est1.codigo = "7502520040";
         est1.semestre = 2;
 
+
         est1.mostrarInfo();
-
+        est1.cambiarSemestre(3);
+        est1.mostrarInfo();
     }
-
-
-
-
 }
