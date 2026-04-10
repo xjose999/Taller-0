001519c1e61eeb7a75fe802cdcf9358d9fa0ea16
xjose999 <josejoestar80@gmail.com>
2026-04-09 23:28:20 -0500

    Ejercicio 30

--- Estudiante.java
+++ Estudiante.java
@@ -3,20 +3,17 @@ public class Estudiante {
     String codigo;
     int semestre;
 
-
     public Estudiante() {
         this.nombre = "Sin nombre";
         this.codigo = "0000000000";
         this.semestre = 1;
     }
 
-
     public Estudiante(String nombre, String codigo) {
         this.nombre = nombre;
         this.codigo = codigo;
         this.semestre = 1;
     }
-
 
     public Estudiante(String nombre, String codigo, int semestre) {
         this.nombre = nombre;
@@ -29,18 +26,12 @@ public class Estudiante {
     }
 
     public static void main(String[] args) {
+        Estudiante estVacio = new Estudiante();
+        Estudiante estParcial = new Estudiante("José Daniel", "7502520040");
+        Estudiante estCompleto = new Estudiante("Andrés Castro", "7502520041", 5);
 
-        Estudiante est1 = new Estudiante();
-
-
-        Estudiante est2 = new Estudiante("José Daniel", "7502520040");
-
-
-        Estudiante est3 = new Estudiante("Andrés Castro", "7502520041", 5);
-
-        System.out.println("Lista de Estudiantes");
-        est1.mostrarInfo();
-        est2.mostrarInfo();
-        est3.mostrarInfo();
+        estVacio.mostrarInfo();
+        estParcial.mostrarInfo();
+        estCompleto.mostrarInfo();
     }
 }
