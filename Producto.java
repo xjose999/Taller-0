ffcc0d5b1e96d6042a791e1db76da860ffb6b62e
xjose999 <josejoestar80@gmail.com>
2026-04-09 21:05:58 -0500

    Ejercicio 16

--- Producto.java
+++ Producto.java
@@ -3,45 +3,43 @@ public class Producto {
     int stock;
     int precio;
 
-
     public void mostrarProducto() {
-        System.out.println("---------------------------");
-        System.out.println("Su producto es: " + nombre);
-        System.out.println("En stock hay: " + stock);
-        System.out.println("Su producto tiene un valor de: " + precio);
+        System.out.println("Producto: " + nombre);
+        System.out.println("Stock actual: " + stock);
     }
-
 
     public void vender(int cantidad) {
         if (cantidad <= stock) {
             stock = stock - cantidad;
-            System.out.println("Venta realizada: " + cantidad + " unidades de " + nombre);
+            System.out.println("Venta realizada: " + cantidad + " unidades.");
         } else {
-            System.out.println("Error: No hay suficiente stock de " + nombre + ". Intento de venta: " + cantidad + " | Disponible: " + stock);
+            System.out.println("Error: No hay suficiente stock.");
+        }
+    }
+
+
+    public void reabastecer(int cantidad) {
+        if (cantidad > 0) {
+            stock = stock + cantidad;
+            System.out.println("Reabastecimiento exitoso: Se sumaron " + cantidad + " unidades.");
+        } else {
+            System.out.println("Error: La cantidad a reabastecer debe ser mayor a 0.");
         }
     }
 
     public static void main (String[] args) {
-
         Producto product1 = new Producto();
         product1.nombre = "Juguete Max Steel";
-        product1.stock = 60;
+        product1.stock = 10;
         product1.precio = 200;
 
-
-        Producto product2 = new Producto();
-        product2.nombre = "Juguete Barbie";
-        product2.stock = 25;
-        product2.precio = 250;
-
-
-
-
-        product1.vender(10);
         product1.mostrarProducto();
 
 
-        product2.vender(30);
-        product2.mostrarProducto();
+        System.out.println("\n--- Llegó un pedido de fábrica ---");
+        product1.reabastecer(50);
+
+
+        product1.mostrarProducto();
     }
 }
